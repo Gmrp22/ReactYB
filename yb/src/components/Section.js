bf9e0f9b3e327f8c0973d630f02cd14c40d87039
Gilda <gmrp22@hotmail.com>
2020-11-26 15:40:00 -0600

    cambios

--- yb/src/components/Section.js
+++ yb/src/components/Section.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./styles/section.css";
 import Thumbnail from "./Thumbnail.js";
-
+import InfoVideo from "./InfoVideo.js";
 class Section extends React.Component {
   render() {
     return (
       <section class="video-section">
         <article class="video-container">
           <Thumbnail />
-
+          <InfoVideo />
         </article>
       </section>
     );
